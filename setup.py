d62f6087a735af2218616adaea0e9dba32514cc5
Shivanshu Singh <shivanshusinghrajput29@gmail.com>
2023-05-29 21:49:05 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VirtualBookshelf',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/shivanshurajput/VirtualBookshelf',
+    license='',
+    author='Shivanshu',
+    author_email='shivanshusinghrajput29@gmail.com',
+    description=''
+)
